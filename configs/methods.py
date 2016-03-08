327d7134ec336704db089c4d2387fc8b9b2182f0
greenteamer <greenteamer@bk.ru>
2016-03-08 22:24:40 +0300

    deploy to server

--- configs/methods.py
+++ configs/methods.py
@@ -5,5 +5,4 @@ def get_site_config(request):
 	try:
 		return Config.objects.get(site__domain=request.get_host())
 	except Exception:
-		print(u'Конфигурация сайта не создана.')
 		return Config.objects.get(site__domain="example.com")
