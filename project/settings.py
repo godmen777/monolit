8a4d527540e3a00d3db84d4abdbdd14cffd0e35e
greenteamer <greenteamer@bk.ru>
2016-03-19 15:35:39 +0300

    suit admin

--- project/settings.py
+++ project/settings.py
@@ -31,10 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'suit',
-    'material',
-    'material.frontend',
-    'material.admin',
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
