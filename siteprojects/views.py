d1d1e490758e4d225eccf539359ac0534f424110
greenteamer <greenteamer@bk.ru>
2016-05-05 00:14:30 +0300

    services done, project_list done

--- siteprojects/views.py
+++ siteprojects/views.py
@@ -3,10 +3,17 @@ from django.template import RequestContext
 
 
 from siteprojects.models import Project, ProjectImage
+from core.models import Post
 
 
 def project_item(request, slug, template_name="siteprojects/project_item.html"):
 	project = Project.objects.get(slug=slug)
 	performer_profile = project.account.performer_of_account
 	featured_projects = Project.objects.filter(featured=True)
+	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
+
+def project_list(request, template_name="siteprojects/project_list.html"):
+	projects = Project.objects.all()
+	posts = Post.objects.all()[:5]
 	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
