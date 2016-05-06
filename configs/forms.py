a39ba5f4f5da302110b4f98f356f82d27b7b8034
Lehych <you@example.com>
2016-05-07 01:09:45 +0300

    forma

--- configs/forms.py
+++ configs/forms.py
@@ -2,6 +2,40 @@
 from django import forms
 from django.core.mail import send_mail
 from methods import get_site_config
+from siteprojects.models import Project
+
+
+class ProjectForm(forms.Form):
+	def __init__(self, *args, **kwargs):
+		super(ProjectForm, self).__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs = {'placeholder':'Ваше имя', 'class':'form-control'}
+		self.fields['phone'].widget.attrs = {'placeholder':'Ваш телефон', 'class':'form-control'}
+		self.fields['name'].label = ""
+		self.fields['phone'].label = ""
+	name = forms.CharField()
+	phone = forms.CharField()
+	project = forms.IntegerField(widget=forms.HiddenInput)
+
+	class Meta:
+		fields = [
+			'name',
+			'phone',
+			'project',
+		]
+		labels = {
+			"name": u"",
+			"phone": u""
+		}
+
+	def send_email(self, request):
+		data = self.cleaned_data
+		# получаем данные конфигурации сайта
+		config = get_site_config(request)
+		project = Project.objects.get(id=data['project'])
+		# отправка формы
+		subject = u'Контактные данные пользователя %s' % config.site.domain
+		message = u'Имя: %s \n телефон: %s \n Название проекта: %s' % (data['name'], data['phone'] , project.name)
+		send_mail(subject, message, 'teamer777@gmail.com', [config.site_email], fail_silently=False)
 
 
 class ContactForm(forms.Form):
--- siteprojects/views.py
+++ siteprojects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from configs.forms import ProjectForm
 
 
 from siteprojects.models import Project, ProjectImage
@@ -10,6 +11,11 @@ def project_item(request, slug, template_name="siteprojects/project_item.html"):
 	project = Project.objects.get(slug=slug)
 	performer_profile = project.account.performer_of_account
 	featured_projects = Project.objects.filter(featured=True)
+	form = ProjectForm(initial={'project':project.id,})
+	if request.POST:
+		form = ProjectForm(request.POST)
+		if form.is_valid():
+			form.send_email(request)
 	return render_to_response(template_name, locals(), context_instance=RequestContext(request))
 
 
